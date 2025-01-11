f26ae6ad4189b6acd742dff13e81385da2b4eae6
a-t-h-a-r-v <atharvsnehalshirgurkar@gmail.com>
2025-01-11 15:29:33 +0530

    Added linking of sway and waybar

--- setup.sh
+++ setup.sh
@@ -3,4 +3,6 @@ ln -sf "$DOTFILES_DIR/.vimrc" ~/.vimrc
 ln -sf "$DOTFILES_DIR/.vim" ~/.vim
 ln -sf "$DOTFILES_DIR/.config/nvim" ~/.config/nvim
 ln -sf "$DOTFILES_DIR/.config/hyprland" ~/.config/hyprland
+ln -sf "$DOTFILES_DIR/.config/waybar" ~/.config/waybar
+ln -sf "$DOTFILES_DIR/.config/sway" ~/.config/sway
 echo "Dotfiles setup complete!"
