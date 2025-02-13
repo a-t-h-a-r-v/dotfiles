269c0a16e78a6f41c82338fd59af7ff2ae8e6875
a-t-h-a-r-v <atharvsnehalshirgurkar@gmail.com>
2025-02-13 16:05:15 +0530

    Added boilerplates handling for cmake and qmake

--- .config/nvim/lua/boilerplate.lua
+++ .config/nvim/lua/boilerplate.lua
@@ -1,19 +1,26 @@
 -- Function to insert the boilerplate into a new file
 local function insert_boilerplate()
-    local filename = vim.fn.expand("%:p") -- Get full file path of the new file
+    local filename = vim.fn.expand("%:p") -- Get full file path
+    local basename = vim.fn.expand("%:t") -- Get just the filename
     local ext = vim.fn.expand("%:e") -- Get file extension
 
-    if vim.fn.filereadable(filename) == 0 then -- Check if the file is empty or does not exist
-        local template_path = "~/.config/nvim/boilerplates/template." .. ext
-        if vim.fn.filereadable(vim.fn.expand(template_path)) == 1 then
-            vim.cmd("0r " .. template_path) -- Read and insert the template content at the top
-            -- Remove extra empty line at the end if exists
-            vim.cmd([[%s/\n\+\%$//e]])
-        end
+    local template_path
+
+    -- Use a special template for CMakeLists.txt
+    if basename == "CMakeLists.txt" then
+        template_path = "~/.config/nvim/boilerplates/CMakeLists.txt"
+    else
+        template_path = "~/.config/nvim/boilerplates/template." .. ext
+    end
+
+    -- Check if file exists and is empty before inserting the template
+    if vim.fn.filereadable(filename) == 0 and vim.fn.filereadable(vim.fn.expand(template_path)) == 1 then
+        vim.cmd("0r " .. template_path) -- Read and insert the template content at the top
+        vim.cmd([[%s/\n\+\%$//e]]) -- Remove extra empty line at the end if exists
     end
 end
 
--- Set up autocmd to run the function on buffer creation or reading
+-- Set up autocmd to run the function on buffer creation
 vim.api.nvim_create_autocmd("BufNewFile", {
     pattern = "*",
     callback = insert_boilerplate,
