0666d7cf2c4c89efc9ff6d54b02f536488e9526f
a-t-h-a-r-v <atharvsnehalshirgurkar@gmail.com>
2025-03-22 16:54:25 +0530

    Edited neovim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,77 +1,180 @@
--- Set leader keys
+-- Ensure packer is loaded
+vim.cmd [[packadd packer.nvim]]
+
+-- Set tab to 2 spaces
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.smartindent = true
+vim.o.termguicolors = true
+
+-- Show line numbers and relative line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Set space as the leader key
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
--- Basic Settings
-vim.o.number = true
-vim.o.relativenumber = true
-vim.o.termguicolors = true
-vim.o.tabstop = 4
-vim.o.softtabstop = 4
-vim.o.shiftwidth = 4
-vim.o.expandtab = true
+-- Set up packer
+require('packer').startup(function(use)
+  -- Packer manages itself
+  use 'wbthomason/packer.nvim'
 
--- Plugin Manager (packer)
-require('packer').startup(function()
-    use 'wbthomason/packer.nvim'
-    
-    -- Autocompletion
-    use {'neoclide/coc.nvim', branch = 'release'}
-    
-    -- Colorscheme
-    use 'catppuccin/nvim'
-    
-    -- LSP (via coc.nvim extensions)
-    vim.g.coc_global_extensions = {
-        'coc-tsserver', 'coc-html', 'coc-css', 'coc-java', 'coc-pyright', 'coc-clangd', 'coc-sh'
-    }
-    
-    use {
-        'windwp/nvim-autopairs',
-        config = function()
-            require('nvim-autopairs').setup {
-                -- Add your custom settings here if needed
-            }
+  -- LSP support
+  use 'neovim/nvim-lspconfig'
 
-            -- If using nvim-cmp, integrate autopairs with it
-            local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-            local cmp = require('cmp')
-            cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
-        end
-    }
-    
-    -- Emmet
-    use 'mattn/emmet-vim'
-    
-    -- Git Integration
-    use 'tpope/vim-fugitive'
-    
-    -- Status Line
-    use 'vim-airline/vim-airline'
-    use 'vim-airline/vim-airline-themes'
-    
-    -- Fuzzy File Search
-    use {'junegunn/fzf', run = function() vim.fn['fzf#install']() end}
-    use 'junegunn/fzf.vim'
-    
-    -- File Explorer
-    use 'preservim/nerdtree'
+  -- Autocompletion plugins
+  use 'hrsh7th/nvim-cmp'
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'L3MON4D3/LuaSnip'
+
+  -- Auto-pairing brackets and quotes
+  use 'windwp/nvim-autopairs'
+  use 'tpope/vim-surround'
+
+  -- Recent files plugin
+  use 'mhinz/vim-startify'
+
+  -- Catppuccin theme
+  use { 'catppuccin/nvim', as = 'catppuccin' }
+
+  -- Add lualine status bar
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+  }
+
+  use 'tpope/vim-fugitive'
+
+  -- Fuzzy File Search
+  use {'junegunn/fzf', run = function() vim.fn['fzf#install']() end}
+  use 'junegunn/fzf.vim'
+
+  -- File Explorer
+  use 'preservim/nerdtree'
+
+  -- Automatically sync plugins after cloning packer.nvim
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
 
--- Colorscheme Setup
-vim.cmd [[colorscheme catppuccin-macchiato]]
-vim.cmd [[hi Normal guibg=NONE ctermbg=NONE]]
-vim.g.airline_theme = 'catppuccin'
-vim.g.airline_powerline_fonts = 1
+-- Configure LSP for C/C++ with clangd
+local lspconfig = require('lspconfig')
 
--- Key Mappings for coc.nvim
-vim.api.nvim_set_keymap('n', '<leader>gd', '<Plug>(coc-definition)', {})
-vim.api.nvim_set_keymap('n', '<leader>gr', '<Plug>(coc-references)', {})
-vim.api.nvim_set_keymap('n', '<leader>rn', '<Plug>(coc-rename)', {})
+lspconfig.clangd.setup({
+  cmd = { 'clangd' },
+  filetypes = { 'c', 'cpp', 'objc', 'objcpp' },
+  root_dir = lspconfig.util.root_pattern('.clangd', 'compile_commands.json', 'compile_flags.txt', '.git'),
+  settings = {
+    clangd = {
+      fallbackFlags = { '-std=c++20' },
+      completion = {
+        detailedLabel = true
+      } 
+    },
+  },
+  on_attach = function(client, bufnr)
+    local opts = { noremap=true, silent=true, buffer=bufnr }
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
 
--- Use <Tab> for both indent and completion
-vim.api.nvim_set_keymap('i', '<Tab>', "pumvisible() ? '<C-n>' : '<Tab>'", {expr = true})
-vim.api.nvim_set_keymap('i', '<S-Tab>', "pumvisible() ? '<C-p>' : '<S-Tab>'", {expr = true})
+    -- Automatically show parameter hints
+    vim.cmd [[autocmd CursorHoldI * lua vim.lsp.buf.signature_help()]]
+  end
+})
+
+-- Setup nvim-cmp for autocompletion
+local cmp = require('cmp')
+
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ['<Tab>'] = cmp.mapping.select_next_item(),
+    ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+  }, {
+    { name = 'buffer' },
+  })
+})
+
+-- Setup nvim-autopairs
+require('nvim-autopairs').setup({
+  check_ts = true,
+})
+
+-- Set Catppuccin theme (Macchiato variant)
+require('catppuccin').setup({
+  flavour = 'macchiato',
+  integrations = {
+    nvimtree = true,
+    treesitter = true,
+    cmp = true,
+    lsp_trouble = true,
+    telescope = true,
+  },
+})
+vim.cmd.colorscheme 'catppuccin'
+
+-- Keybinding for easier LSP info access
+vim.api.nvim_set_keymap('n', '<leader>li', ':LspInfo<CR>', { noremap = true, silent = true })
+
+-- Load LuaSnip snippets for boilerplates
+require('luasnip.loaders.from_lua').load({ paths = '~/.config/nvim/lua/snippets/' })
+
+-- Auto-insert boilerplate templates for new files
+local function insert_boilerplate()
+  local filename = vim.fn.expand('%:t')
+  local extension = vim.fn.expand('%:e')
+  local template_path = string.format('~/.config/nvim/boilerplates/template.%s', extension)
+
+  if vim.fn.filereadable(vim.fn.expand(template_path)) == 1 then
+    local content = vim.fn.readfile(vim.fn.expand(template_path))
+    vim.api.nvim_buf_set_lines(0, 0, -1, false, content)
+  end
+end
+
+vim.api.nvim_create_autocmd('BufNewFile', {
+  pattern = '*.*',
+  callback = insert_boilerplate
+})
+
+-- Compile C/C++ files with Make or fallback to gcc/clang
+vim.api.nvim_set_keymap('n', '<leader>cc', 
+":w<CR>:lua if vim.fn.filereadable('Makefile') == 1 then vim.cmd('!make') else vim.cmd('!gcc % -o %< && ./%<') end<CR>", 
+{ noremap = true, silent = true })
+
+-- Setup lualine with Catppuccin theme
+require('lualine').setup({
+  options = {
+    theme = 'catppuccin',
+    icons_enabled = true,
+    component_separators = { left = '', right = '' },
+    section_separators = { left = '', right = '' },
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {'filename'},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
+  extensions = {'quickfix', 'fugitive'}
+})
 
 -- Key Mappings for fzf
 vim.api.nvim_set_keymap('n', '<leader>ff', ':Files<CR>', {noremap = true})
@@ -82,39 +185,3 @@ vim.api.nvim_set_keymap('n', '<C-f>', ':Rg<space>', {noremap = true})
 
 -- Open NERDTree
 vim.api.nvim_set_keymap('n', '<leader>e', ':NERDTreeToggle<CR>', {noremap = true})
-
--- Boilerplate Insertion
-function InsertBoilerplate()
-    local filename = vim.fn.expand("%:p")
-    local basename = vim.fn.expand("%:t")
-    local ext = vim.fn.expand("%:e")
-    local template_path = os.getenv("HOME") .. "/.config/nvim/boilerplates/template." .. ext
-    
-    if basename == "CMakeLists.txt" then
-        template_path = os.getenv("HOME") .. "/.config/vim/boilerplates/CMakeLists.txt"
-    end
-    
-    if vim.fn.filereadable(filename) == 0 and vim.fn.filereadable(template_path) == 1 then
-        vim.cmd("0r " .. template_path)
-    end
-end
-vim.api.nvim_create_autocmd("BufNewFile", {pattern = "*", callback = InsertBoilerplate})
-
--- Search word in browser
-function SearchWordInBrowser()
-    local word = vim.fn.expand("<cword>")
-    local url = "https://www.google.com/search?q=" .. word
-    os.execute("xdg-open " .. url .. " &")
-end
-vim.api.nvim_set_keymap('n', '<Leader>s', ':lua SearchWordInBrowser()<CR>', {noremap = true})
-
--- Search selection in browser
-function SearchSelectionInBrowser()
-    local old_reg = vim.fn.getreg('"')
-    vim.cmd('normal! gvy')
-    local selection = vim.fn.getreg('"')
-    local url = "https://www.google.com/search?q=" .. vim.fn.escape(selection, " ")
-    os.execute("xdg-open " .. url .. " &")
-    vim.fn.setreg('"', old_reg)
-end
-vim.api.nvim_set_keymap('v', '<Leader>gs', ':lua SearchSelectionInBrowser()<CR>', {noremap = true})
